5fbcb48dc606b89cca1e50e5f1c60a295a691a2f
Bushra Addlan <bushra.addlan@gmail.com>
2020-01-14 23:23:47 +0300

    refactor(exports): add Chip class to root exports

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import './index.css';
 
 export {ChipContainer as ChipContainer} from './chip-container';
+export {Chip as Chip} from './chip';
 
 // Using named exports because Rollup hicks up when exporting an interface.
 import {ChipContainerProps as _ChipContainerProps} from './chip-container';
