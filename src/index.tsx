fdd7c3457d7fe7838b36ab5fefe201a01d1c58c2
Bushra Addlan <bushra.addlan@gmail.com>
2020-01-23 00:02:05 +0300

    fix(typings): expose more types from package root

--- src/index.tsx
+++ src/index.tsx
@@ -6,3 +6,10 @@ export {Chip as Chip} from './chip';
 // Using named exports because Rollup hicks up when exporting an interface.
 import {ChipContainerProps as _ChipContainerProps} from './chip-container';
 export type ChipContainerProps = _ChipContainerProps;
+
+export {
+  ContainerChangeEvent,
+  ChipProps,
+  IChip,
+  DraggableItem,
+} from './types';
