74d85494b0e52ee91188d0e2bd64719dab230bb1
Bushra Addlan <bushra.addlan@gmail.com>
2020-01-23 17:45:29 +0300

    refactor(typings): use named exports for TS interfaces

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
+import {ChipContainerProps as _ChipContainerProps} from './chip-container';
+import {
+  ChipProps as _ChipProps,
+  ContainerChangeEvent as _ContainerChangeEvent,
+  DraggableItem as _DraggableItem,
+  IChip as _IChip,
+} from './types';
 import './index.css';
+
 
 export {ChipContainer as ChipContainer} from './chip-container';
 export {Chip as Chip} from './chip';
 
 // Using named exports because Rollup hicks up when exporting an interface.
-import {ChipContainerProps as _ChipContainerProps} from './chip-container';
-export type ChipContainerProps = _ChipContainerProps;
-
-export {
-  ContainerChangeEvent,
-  ChipProps,
-  IChip,
-  DraggableItem,
-} from './types';
+export type ChipProps = _ChipProps;
+export type ContainerChangeEvent = _ContainerChangeEvent;
+export type DraggableItem = _DraggableItem;
+export type IChip = _IChip;
